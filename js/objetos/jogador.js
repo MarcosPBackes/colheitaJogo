3689ce00a9f6277acd180f140598f13b370a0151
MarcosPBackes <mpb.inv@gmail.com>
2023-03-13 22:07:30 -0300

    Adicionado movimentaçao do personagem para cada direçao

--- js/objetos/jogador.js
+++ js/objetos/jogador.js
@@ -1,45 +1,98 @@
-import { loadImage } from '../utils/loader.js';
-
 export class Player {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.image = null;
-    this.speed = 5;
-    this.loadImage();
-  }
-  async loadImage() {
-    this.image = await loadImage('./img/npcs/personagem_baixo.png');
-  }
-  draw(ctx, cameraX, cameraY) {
-    const spriteWidth = this.image.width / 4;
-    const spriteHeight = this.image.height;
-    ctx.drawImage(
-      this.image,
-      0,
-      0,
-      spriteWidth,
-      spriteHeight,
-      this.x - spriteWidth / 2 + cameraX,
-      this.y - spriteHeight / 2 + cameraY,
-      spriteWidth,
-      spriteHeight
-    );
-  }
+    constructor(x, y, image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.speed = 18;
+        //this.posIniSprite = 0;
+        this.linhaSprite = 0;
+        this.colunaSprite = 0;
+        //this.numeroSprite = 0;
+        this.quantSprite = 4;
+        this.tempoUltimoSprite = 0;
+        this.tempoTrocaSprite = 150;
+    }
+  
+    draw(ctx, cameraX, cameraY) {
+        const spriteWidth = this.image.width / this.quantSprite;
+        const spriteHeight = this.image.height / this.quantSprite;
+        ctx.drawImage(
+            this.image,
+            spriteWidth * this.colunaSprite,
+            spriteHeight * this.linhaSprite,
+            spriteWidth,
+            spriteHeight, 
+            this.x - spriteWidth / 2 + cameraX, 
+            this.y - spriteHeight / 2 + cameraY, 
+            spriteWidth, 
+            spriteHeight
+        );
+    }    
+    
+    
+    moveDown() {         
+        this.y += this.speed;
+        if (this.linhaSprite == 0) {
+            if (Date.now() - this.tempoUltimoSprite > this.tempoTrocaSprite) {
+                this.tempoUltimoSprite = Date.now();
+                this.colunaSprite ++;
+                if(this.colunaSprite > 3) {
+                    this.colunaSprite = 0;
+            }
+        } 
+        } else {
+            this.linhaSprite = 0;
+            this.colunaSprite = 0;
+        }       
+    }
 
-  moveUp() {
-    this.y -= this.speed;
+    moveUp() {
+        this.y -= this.speed;
+        if (this.linhaSprite == 1) {
+            if (Date.now() - this.tempoUltimoSprite > this.tempoTrocaSprite) {
+                this.tempoUltimoSprite = Date.now();
+                this.colunaSprite ++;
+                if(this.colunaSprite > 3) {
+                    this.colunaSprite = 0;
+            }
+        } 
+        } else {
+            this.linhaSprite = 1;   
+            this.colunaSprite = 0;         
+        }        
+      }
+    
+  
+    moveLeft() {
+        this.x -= this.speed;
+        if (this.linhaSprite == 2) {
+            if (Date.now() - this.tempoUltimoSprite > this.tempoTrocaSprite) {
+                this.tempoUltimoSprite = Date.now();
+                this.colunaSprite ++;
+                if(this.colunaSprite > 3) {
+                    this.colunaSprite = 0;
+            }
+        } 
+        } else {
+            this.linhaSprite = 2;
+            this.colunaSprite = 0;
+        }       
+    }
+  
+    moveRight() {
+        this.x += this.speed;
+        if (this.linhaSprite == 3) {
+            if (Date.now() - this.tempoUltimoSprite > this.tempoTrocaSprite) {
+                this.tempoUltimoSprite = Date.now();
+                this.colunaSprite ++;
+                if(this.colunaSprite > 3) {
+                    this.colunaSprite = 0;
+            }
+        } 
+        } else {
+            this.linhaSprite = 3;
+            this.colunaSprite = 0;
+        }
+    }
   }
-
-  moveDown() {
-    this.y += this.speed;
-  }
-
-  moveLeft() {
-    this.x -= this.speed;
-  }
-
-  moveRight() {
-    this.x += this.speed;
-  }
-}
+  
--- js/objetos/mapa.js
+++ js/objetos/mapa.js
@@ -1,18 +1,11 @@
-import { loadImage } from '../utils/loader.js';
-
 export class Village {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.image = null;
-    this.loadImage();
-  }
-
-  async loadImage() {
-    this.image = await loadImage('./img/maps/mapa_vilarejo_01.png');
+  constructor(x, y, image) {
+      this.x = x;
+      this.y = y;
+      this.image = image;
   }
 
   draw(ctx, cameraX, cameraY) {
-    ctx.drawImage(this.image, this.x + cameraX, this.y + cameraY);
+      ctx.drawImage(this.image, this.x + cameraX, this.y + cameraY);
   }
 }
